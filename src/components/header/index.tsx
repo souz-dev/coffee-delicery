3df45554520b505c142bf20211c80e2409f2e723
souz-dev <souz.developer@gmail.com>
2023-08-23 00:59:34 -0300

    feat: :rocket: finish layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,18 @@
 import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
 import {
   CartButton,
   LocationButton,
   HeaderContainer,
   HeaderButtonsContainer,
+  CartButtonSection,
+  CartButtonCounter,
 } from './styled'
 import { MapPin, ShoppingCart } from 'phosphor-react'
+import { useCart } from '../../hooks/useCart'
 export function Header() {
+  const { quantity } = useCart()
   return (
     <HeaderContainer>
       <div className="container">
@@ -16,9 +22,14 @@ export function Header() {
             <MapPin size={16} weight="fill" />
             Gamnambi, BA
           </LocationButton>
-          <CartButton>
-            <ShoppingCart size={16} weight="fill" />
-          </CartButton>
+          <NavLink to="/completeOrder">
+            <CartButtonSection>
+              <CartButton>
+                <ShoppingCart size={16} weight="fill" />
+              </CartButton>
+              {<CartButtonCounter>{quantity}</CartButtonCounter>}
+            </CartButtonSection>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
